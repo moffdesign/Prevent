548b312c1062319713f7a15bb72dde787cfb7391
Guillaume NOAH <guillaumenoah@gmail.com>
2025-05-08 13:53:14 +0200

    maj dbcontext

--- Prevent.DAL/PreventDbContext.cs
+++ Prevent.DAL/PreventDbContext.cs
@@ -17,36 +17,48 @@ namespace Prevent.DAL
 		}
 
 		public DbSet<Activite> Activites { get; set; }
-		public DbSet<Risque> Risques { get; set; }
-		public DbSet<MesurePrevention> Preventions { get; set; }
-		public DbSet<Employe> Employes { get; set; }
-		public DbSet<Espace> Espaces { get; set; }
-		public DbSet<Evenement> Evenements { get; set; }
+		public DbSet<ActiviteReferencee> ActivitesReferencees { get; set; }
+		public DbSet<ActiviteRisque> ActivitesRisques { get; set; }
 		public DbSet<Audit> Audits { get; set; }
+
+        //public DbSet<DetailSignalement> DetailsSignalement { get; set; }
+        //public DbSet<EffetInduit> EffetsInduits { get; set; }
+        public DbSet<Employe> Employes { get; set; }
+        public DbSet<EmployeReference> EmployesReferences { get; set; }
+        public DbSet<Espace> Espaces { get; set; }
+        public DbSet<Evenement> Evenements { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<PlanRisque> PlansRisques { get; set; }
+		public DbSet<PorteeAudit> PorteesAudits { get; set; }
+		public DbSet<PorteeEvenement> PorteesEvenements { get; set; }
+		public DbSet<PorteePlan> PorteesPlans { get; set; }
+		
+		public DbSet<Prevention> Preventions { get; set; }
+		public DbSet<PreventionReferencee> PreventionsReferencees { get; set; }
+		public DbSet<PreventionRisque> PreventionsRisques { get; set; }
+		public DbSet<Risque> Risques { get; set; }
+		public DbSet<RisqueReference> RisquesReferencees { get; set; }
 		public DbSet<Signalement> Signalements { get; set; }
-		public DbSet<PlanAction> PlanActions { get; set; }
+		
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			modelBuilder.ApplyConfiguration(new FluentConfiguration.PorteeAudit_FluentConfiguration());
-			modelBuilder.ApplyConfiguration(new FluentConfiguration.ActiviteRisque_FluentConfiguration());
-			//modelBuilder.Entity<Brand>().HasData(new Brand { BrandId = 1, BrandName = "Brand 1" });
+            //modelBuilder.ApplyConfiguration(new FluentConfiguration.Audit_FluentConfiguration());
 
-			/*
-			modelBuilder.Entity<ActiviteRisque>()
-				.HasKey(ra => new { ra.ActiviteId, ra.RisqueId });
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.ActiviteRisque_FluentConfiguration());
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.ActiviteReferencee_FluentConfiguration());
 
-			modelBuilder.Entity<ActiviteRisque>()
-				.HasOne(a => a.Risque)
-				.WithMany()
-				.HasForeignKey(ra => ra.RisqueId);
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.EmployeReference_FluentConfiguration());
 
-			modelBuilder.Entity<ActiviteRisque>()
-				.HasOne(a => a.Activite)
-				.WithMany()
-				.HasForeignKey(ra => ra.ActiviteId);
-			*/
-		}
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.PorteeAudit_FluentConfiguration());
+			modelBuilder.ApplyConfiguration(new FluentConfiguration.PorteeEvenement_FluentConfiguration());
+			modelBuilder.ApplyConfiguration(new FluentConfiguration.PorteePlan_FluentConfiguration());
+
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.PreventionReferencee_FluentConfiguration());
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.PreventionRisque_FluentConfiguration());
+            modelBuilder.ApplyConfiguration(new FluentConfiguration.RisqueReference_FluentConfiguration());
+
+        }
 
 		// 1. add-migration amazingMigration
 		// 2. update-database
